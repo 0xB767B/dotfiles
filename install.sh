40b743786316c14ad91032db9230d2af8580b868
0xB767B <marco.schuler@gmail.com>
2015-08-21 14:18:11 +0200

    renamed .bashrc

--- install.sh
+++ install.sh
@@ -13,10 +13,8 @@ echo $DOTFILES_DIR
 git submodule update --init
 
 # create of symlinks
-ln -sfv "$DOTFILES_DIR/bash/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/bash/.bash_logout" ~
 ln -sfv "$DOTFILES_DIR/bash/.bash_aliases" ~
-ln -sfv "$DOTFILES_DIR/bash/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/bash/bashrc" ~/.bashrc
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/vimrc" ~/.vimrc
 ln -sfv "$DOTFILES_DIR/vim" ~/.vim
