a73af46deb7a84f43d58de0b90e70042c84e93ef
0xB767B <marco.schuler@gmail.com>
2016-07-07 15:04:23 +0200

    add gvimrc

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ ln -sfv "$DOTFILES_DIR/profile" ~/.profile
 ln -sfv "$DOTFILES_DIR/bash_logout" ~/.bash_logout
 ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
 ln -sfv "$DOTFILES_DIR/vimrc" ~/.vimrc
+ln -sfv "$DOTFILES_DIR/gvimrc" ~/.gvimrc
 ln -sfv "$DOTFILES_DIR/vim" ~/.vim
 ln -sfv "$DOTFILES_DIR/tmux.conf" ~/.tmux.conf
 ln -sfv "$DOTFILES_DIR/bin" ~/bin
